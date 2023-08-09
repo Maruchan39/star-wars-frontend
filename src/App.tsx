bae93677ab70d546e574c34e09bb9f7f2ef089f7
Martynas Baltutis <martynas.baltutis@zenitech.co.uk>
2023-08-09 22:21:17 +0300

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { About } from "./pages/About";
+import { Films } from "./pages/Films";
+
 function App() {
-  return <h1>Star Wars Frontend</h1>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Films />,
+    },
+    {
+      path: "/about",
+      element: <About />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/About.tsx
@@ -0,0 +1,9 @@
+export const About = () => {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptas
+      quisquam cupiditate asperiores officia magnam vero sed aliquid quas ullam.
+      Quae maiores corrupti quasi autem impedit magni eveniet rerum veniam.
+    </p>
+  );
+};
