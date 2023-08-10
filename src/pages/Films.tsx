be3c7c80f0b6dc4cd02282115fff8567f0c56682
Martynas Baltutis <martynas.baltutis@zenitech.co.uk>
2023-08-10 10:22:46 +0300

    Get swapi data

--- src/pages/Films.tsx
+++ src/pages/Films.tsx
@@ -1,3 +1,18 @@
+async function logMovies() {
+  try {
+    const response = await fetch("https://swapi.dev/api/films");
+    if (!response.ok) {
+      throw new Error("Network response was not OK");
+    }
+
+    const movies = await response.json();
+    console.log(movies);
+  } catch (error) {
+    console.error("There has been a problem with your fetch operation:", error);
+  }
+}
+
 export const Films = () => {
+  logMovies();
   return <p>Films</p>;
 };
